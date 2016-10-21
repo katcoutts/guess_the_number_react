e4d8b27f2eb04d354f098a030bcd4e8e0355551e
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-10-21 09:02:54 +0100

    files created and initial json data written

---
+++ client/src/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+window.onload = function(){
+  ReactDOM.render(
+    <div>
+    <h1> Guess The Number </h1>
+    <img src="./images/number_19.jpg"/>
+    <img src="./images/number_07.jpg"/>
+    <img src="./images/number_08.jpg"/>
+    <img src="./images/number_09.jpg"/>
+    <img src="./images/number_16.jpg"/>
+    </div>,
+    document.getElementById('app')
+  );
+}
